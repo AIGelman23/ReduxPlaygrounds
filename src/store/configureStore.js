6667bcbe21efc2c1544efb4e0a98b0abdaf71368
AIGelman23 <AIGelman23@gmail.com>
2021-02-03 22:08:15 -0500

    Redux Tool Kit 1.0

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,11 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+
 import reducer from './bugs';
 
 
-export default function configureStore() {
-  const store = createStore(
-    reducer, 
-    devToolsEnhancer({ trace: true })
-  );
+export default function() {
+  const store = configureStore({
+    reducer
+  });
   return store;
 }
